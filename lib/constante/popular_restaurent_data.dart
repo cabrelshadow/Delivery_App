0feace8bc5c0a59e67dac5f64b95b5b83943a854
mr shadow <sianou93@gmail.com>
2024-04-12 20:18:33 +0200

    must popular and and list data for each card

---
+++ lib/constante/popular_restaurent_data.dart
@@ -0,0 +1,21 @@
+import 'image_string.dart';
+
+List pupular_Restaurent=
+[
+  {
+    "image":res_1,
+    "Title":"Minute by tuk tuk",
+    "subtitle":"124 ratings",
+    "subtitle3":"Western Food",
+    "grade":"4.5"
+
+  },
+  {
+    "image":res_2,
+    "Title":"Akamara fast food",
+    "subtitle":"124 ratings",
+    "subtitle3":"Western Food",
+    "grade":"4.7"
+
+  }
+];
